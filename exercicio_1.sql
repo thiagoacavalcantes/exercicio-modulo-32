3a0b569cabe9049aca194aa606a83cd7fc710c61
Thiago Cavalcante <thiagoacavalcante_@hotmail.com>
2025-05-26 16:57:46 -0300

    Atualização Exercicio 32

--- exercicio_1.sql
+++ exercicio_1.sql
@@ -9,22 +9,21 @@ LIMIT 100
 
 # -  Faça uma consulta que retorne a lista de todos os atores com o número filmes que cada ator
 # participou. Ordene a lista pelo numero de filmes, iniciando pelos atores que mais atuaram.
-SELECT ac.first_name as ator, COUNT(*) as quantidade_filme FROM actor as ac 
-inner join film_actor as fa
-on ac.actor_id = fa.actor_id
-inner join film as fm
-on fa.film_id = fm.film_id
-GROUP BY ator
-order by quantidade_filme desc
-LIMIT 100
+SELECT ac.first_name || ' ' || ac.last_name AS ator, COUNT(*) AS quantidade_filme
+FROM actor ac
+JOIN film_actor fa ON ac.actor_id = fa.actor_id
+GROUP BY ac.actor_id, ac.first_name, ac.last_name
+ORDER BY quantidade_filme DESC
+LIMIT 100;
+
 
 #Faça uma consulta que retorne a lista de atores que atuaram em filmes com mais de duas horas de
 # duração (120min). Ordene a lista pelo numero de filmes que cada ator participou
 
-SELECT ac.first_name as ator FROM actor as ac 
-inner join film_actor as fa
-on ac.actor_id = fa.actor_id
-inner join film as fm
-on fa.film_id = fm.film_id
+SELECT ac.first_name || ' ' || ac.last_name AS ator, COUNT(*) AS quantidade_filme
+FROM actor ac
+JOIN film_actor fa ON ac.actor_id = fa.actor_id
+JOIN film fm ON fa.film_id = fm.film_id
 WHERE fm.length > 120
-GROUP BY ator
+GROUP BY ac.actor_id, ac.first_name, ac.last_name
+ORDER BY quantidade_filme DESC;
--- exercicio_2.sql
+++ exercicio_2.sql
@@ -8,23 +8,22 @@ inner join category as c
 on fc.category_id = c.category_id
 
 
-SELECT count(total_registros) AS total_linhas
+SELECT COUNT(*) AS total_atores_com_filmes
 FROM (
-  SELECT COUNT(ac.first_name) AS total_registros
+  SELECT ac.actor_id
+  FROM actor AS ac
+  INNER JOIN film_actor AS fa ON ac.actor_id = fa.actor_id
+  GROUP BY ac.actor_id
+) AS subselect;
+
+
+SELECT COUNT(*) AS total_atores_em_filmes_longos
+FROM (
+  SELECT ac.actor_id
   FROM actor AS ac
   INNER JOIN film_actor AS fa ON ac.actor_id = fa.actor_id
   INNER JOIN film AS fm ON fa.film_id = fm.film_id
-  GROUP BY ac.first_name
+  WHERE fm.length > 120
+  GROUP BY ac.actor_id
 ) AS subselect;
 
-SELECT count(total_registros) AS total_linhas
-FROM (
-  	SELECT COUNT(ac.first_name) AS total_registros
-	FROM actor as ac 
-	inner join film_actor as fa
-	on ac.actor_id = fa.actor_id
-	inner join film as fm
-	on fa.film_id = fm.film_id
-	WHERE fm.length > 120
-	GROUP BY ac.first_name
-) AS subselect;
